f940810d4b12068fef107776c7bb9532d0ee1bbd
Lucas Duarte <lucasdu4rte@gmail.com>
2020-03-26 23:51:37 -0300

    feat(web): about page

--- apps/web/src/pages/About/index.js
+++ apps/web/src/pages/About/index.js
@@ -1,12 +1,48 @@
 import React from 'react';
+
 import Header from 'components/Layout/Header';
 import Footer from 'components/Layout/Footer';
-import { Container } from './styles';
+import Button from 'components/Button';
+import aboutImage from 'images/about-image.svg';
+
+import { Container, AboutSection } from './styles';
 
 function About() {
   return (
     <Container>
       <Header />
+      <AboutSection>
+        <div className="container">
+          <div className="row justify-content-lg-center align-items-center">
+            <div className="col-sm-12 col-lg-5">
+              <img src={aboutImage} alt="Imagem sobre o projeto" />
+            </div>
+            <div className="col-sm-12 col-lg-5">
+              <h3>Sobre o projeto</h3>
+              <p className="sub-title">
+                Por que atualizar o estado atual das empresas é tão importante?
+              </p>
+              <p className="description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+
+              <div className="row">
+                <div className="col col-sm-7">
+                  <Button to="/" theme="rose" fontWeight="bold">
+                    Atualize sua empresa
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </AboutSection>
       <Footer />
     </Container>
   );
--- apps/web/src/pages/About/styles.js
+++ apps/web/src/pages/About/styles.js
@@ -1,3 +1,40 @@
 import styled from 'styled-components';
+import colors from 'metrics/colors';
 
 export const Container = styled.div``;
+
+export const AboutSection = styled.section`
+  background: rgba(161, 148, 214, 8%);
+  padding-top: 100px;
+  padding-bottom: 100px;
+  min-height: 90vh;
+
+  img {
+    width: 460px;
+  }
+
+  h3 {
+    text-align: justify;
+    color: rgb(238, 66, 102);
+    font-family: 'Poppins';
+    font-size: 55.8px;
+    font-weight: bolder;
+  }
+
+  .sub-title {
+    text-align: justify;
+    color: ${colors.christalle};
+    font-family: 'Open sans';
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 40px;
+  }
+
+  .description {
+    text-align: justify;
+    color: ${colors.christalle};
+    font-family: 'Open sans';
+    font-size: 18px;
+    margin-bottom: 40px;
+  }
+`;
