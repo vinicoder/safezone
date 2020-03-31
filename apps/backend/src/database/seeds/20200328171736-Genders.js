8ce70ec3be0b04ec83b4e72ba3b098ca3809572f
Lucas Duarte <lucasdu4rte@gmail.com>
2020-03-30 22:11:43 -0300

    Update 20200328171736-Genders.js

--- apps/backend/src/database/seeds/20200328171736-Genders.js
+++ apps/backend/src/database/seeds/20200328171736-Genders.js
@@ -4,20 +4,56 @@ module.exports = {
       'genders',
       [
         {
-          abbreviation: 'FE',
-          description: 'Feminino',
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
           abbreviation: 'MF',
           description: 'Masculino',
           created_at: new Date(),
           updated_at: new Date(),
         },
         {
+          abbreviation: 'FE',
+          description: 'Feminino',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          abbreviation: 'HTG',
+          description: 'Homem trangênero',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          abbreviation: 'MTG',
+          description: 'Mulher trangênero',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          abbreviation: 'HTS',
+          description: 'Homem transexual',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          abbreviation: 'MTS',
+          description: 'Mulher transexual',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          abbreviation: 'NSR',
+          description: 'Não sei responder',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          abbreviation: 'PNR',
+          description: 'Prefiro não responder',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
           abbreviation: 'NB',
-          description: 'Não binário',
+          description: 'Outro',
           created_at: new Date(),
           updated_at: new Date(),
         },
